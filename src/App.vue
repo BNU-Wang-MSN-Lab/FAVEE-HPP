546a8c168329ed2684d36d314c09a159eb393774
mupsy <amm@muxinsq.com>
2023-02-07 11:38:28 +0800

    v1

--- src/App.vue
+++ src/App.vue
@@ -21,25 +21,19 @@ const alertCancel = () => {
 </script>
 
 <template>
+  <fHeader/>
   <div class="container">
-    <RouterView></RouterView>
-    <RouterView name="Favee"></RouterView>
-    <RouterView name="f3d" v-on:alertOpen="alertOpen" class="cc"></RouterView>
-    <RouterView name="Study1" v-on:alertOpen="alertOpen" class="cc"></RouterView>
-    <RouterView name="Study2" v-on:alertOpen="alertOpen" class="cc"></RouterView>
-    <RouterView name="Study3" v-on:alertOpen="alertOpen" class="cc"></RouterView>
-    <RouterView name="region" v-on:alertOpen="alertOpen" class="cc"></RouterView>
+    <RouterView class="cc"></RouterView>
   </div>
   <fAlert :view="show.name" v-on:alertCancel="alertCancel" v-if="show.alert" />
 </template>
 
 <style scoped>
 .container {
-  width: 100%;
-  height: 100%;
-  background: #F5F6F7 repeating-linear-gradient(90deg, transparent 0px, transparent 30px, rgba(0, 0, 255, 0.04) 30px, rgba(0, 0, 255, 0.04) 300px);
+  width: 100vw;
+  background: #F5F6F7;
   background-position: 20px 0px;
-  overflow-y: scroll;
+  overflow-x: hidden;
 }
 
 .cc:nth-child(odd) {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,11 @@
 <script setup>
-import fHeader from "../components/header.vue";
 import { reactive } from 'vue';
 
 const data = reactive({
     href: [
         { name: "GitHub", url: "https://github.com/BNU-Wang-MSN-Lab/FAVEE-HPP" },
         { name: "WangLab Website", url: "http://mirrorneuronwang.com/" },
-        { name: "Contacts", url: "http://mirrorneuronwang.com/" }
+        { name: "Contacts", url: "https://brain.bnu.edu.cn/English/Faculty/CurrentFaculty/Wzz/72f0d1f123b044e1a6f416a02a249fe1.htm" }
     ]
 });
 
@@ -21,71 +20,64 @@ const hrefClick = (e) => {
 
 <template>
     <div class="box">
-        <fHeader/>
-        <div class="bg"></div>
-        <div class="content">
-            <div class="title">
-                Human Relationships
+        <div>
+            <div class="content">
+                <div class="title">
+                    Human Relationships
+                </div>
+                <div style="font-size: 24px; line-height: 30px;">
+                    <p>
+                        Such an enrich social life we are involved in! Image each relationship is a star in the sky, our
+                        human relationship in total is a vast universe. Is there a cognitive map in our mind that guides
+                        us to explore this universe?
+                    </p>
+                    <p>
+                        By collecting data across diverse populations in the world, we found the FAVEE-HPP model. Not
+                        only it is a unified representational space across disciplines, but it also adapted to various
+                        cultures.
+                    </p>
+                </div>
+                <div>
+                    <div v-for="i in data.href" class="button" :data-href="i.url" @click="hrefClick">{{ i.name }}</div>
+                </div>
             </div>
-            <div style="font-size: 24px; line-height: 30px;">
-                <p>
-                    Such an enrich social life we are involved in! Image each relationship is a star in the sky, our human relationship in total is a vast universe. Is there a cognitive map in our mind that guides us to explore this universe?
-                </p>
-                <p>
-                    By collecting data across diverse populations in the world, we found the FAVEE-HPP model. Not only it is a unified representational space across disciplines, but it also adapted to various cultures.
-                </p>
+            <div class="logo">
+                <div></div>
+                <div></div>
             </div>
-            <div>
-                <div v-for="i in data.href" class="button" :data-href="i.url" @click="hrefClick">{{ i.name }}</div>
-            </div>
-        </div>
-        <div class="logo">
-            <div></div>
-            <div></div>
         </div>
     </div>
 </template>
 
 <style scoped>
 div.box {
-    display: block;
-    width: 100%;
-    height: 100%;
-    min-height: 700px;
-    background: #fff repeating-linear-gradient(90deg, transparent 0px, transparent 30px, rgba(0,0,255, 0.04) 30px, rgba(0,0,255, 0.04) 300px);
-    background-position: 20px 0px;
+    display: grid;
+    width: 100vw;
+    min-height: 100vh;
+    grid-template-columns: repeat(10, 1fr);
+    grid-gap: 20px;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+    background-repeat: no-repeat !important;
+    background-image: linear-gradient(90deg, white, transparent), url("/assets/拼图@2x.png") !important;
+    background-size: 533px 813px !important;
+    background-position: right !important;
     position: relative;
 }
-.bg {
-    width: 40%;
-    height: 100%;
-    background: url("/assets/拼图@2x.png") no-repeat;
-    background-size: cover;
-    background-position: center;
-    position: absolute;
-    right: 0;
-}
-.bg::after {
-    content: '';
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(90deg, white, transparent);
-    position: absolute;
-    right: 0;
+.box>div {
+    grid-column: 2 / span 6;
 }
 .content {
-    width: 60%;
-    position: absolute;
-    left: 0;
-    top: calc(50% - 190px);
     text-align: left;
-    margin: 0 0 0 68px;
 }
+
 .title {
     color: var(--theme-color-blue);
     font-size: 64px;
     line-height: 76px;
 }
+
 .button {
     display: inline-block;
     margin: 0 40px 0 0;
@@ -95,21 +87,24 @@ div.box {
     color: var(--theme-color-blue);
     cursor: pointer;
 }
+
 .logo {
-    width: 100%;
-    position: absolute;
-    top: calc(100% - 100px);
+    margin: 30px 0 0 0;
+    text-align: left;
 }
+
 .logo>div {
     display: inline-block;
     width: 72px;
     height: 72px;
     margin: 10px;
 }
+
 .logo>div:nth-child(1) {
     background: url(/assets/icon/Beijing_Normal_University_logo.svg.png) no-repeat;
     background-size: contain;
 }
+
 .logo>div:nth-child(2) {
     background: url(/assets/icon/temple-logo-t-box.svg) no-repeat;
     background-size: contain;
